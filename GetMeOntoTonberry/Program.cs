91dbd153bc95a9aebd509913c80481d69a9d5468
Sam Turner <siberias.samt@gmail.com>
2019-10-06 12:36:57 +1100

    Explicitly handle the 'available' state so that we don't get emails when server maintenance is happening

--- GetMeOntoTonberry/Program.cs
+++ GetMeOntoTonberry/Program.cs
@@ -9,115 +9,119 @@ using System.Net.Mail;
 
 namespace GetMeOntoTonberry
 {
-    class Program
-    {
-        private static string ServerName = "Tonberry";
+	class Program
+	{
+		private static string ServerName = "Tonberry";
 
-        static void Main(string[] args)
-        {
-            QueryServerStatus().GetAwaiter().GetResult();
-        }
+		static void Main(string[] args)
+		{
+			QueryServerStatus().GetAwaiter().GetResult();
+		}
 
-        private static async Task QueryServerStatus()
-        {
-            string url = "https://jp.finalfantasyxiv.com/lodestone/worldstatus/";
-            var httpClient = new HttpClient();
-            string html = await httpClient.GetStringAsync(url);
-            var htmlDocument = new HtmlDocument();
-            htmlDocument.LoadHtml(html);
+		private static async Task QueryServerStatus()
+		{
+			string url = "https://jp.finalfantasyxiv.com/lodestone/worldstatus/";
+			var httpClient = new HttpClient();
+			string html = await httpClient.GetStringAsync(url);
+			var htmlDocument = new HtmlDocument();
+			htmlDocument.LoadHtml(html);
 
-            var worldListItems = htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("world-list__item")).ToList();
+			var worldListItems = htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("world-list__item")).ToList();
 
-            foreach(var item in worldListItems)
-            {
-                var worldNameItems = item.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("world-list__world_name")).ToList();
+			foreach (var item in worldListItems)
+			{
+				var worldNameItems = item.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("world-list__world_name")).ToList();
 
-                if(worldNameItems[0].InnerHtml.Contains(ServerName))
-                {
-                    var characterCreationStatusItems = item.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("world-list__create_character")).ToList();
+				if (worldNameItems[0].InnerHtml.Contains(ServerName))
+				{
+					var characterCreationStatusItems = item.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("world-list__create_character")).ToList();
 
-                    if(characterCreationStatusItems[0].InnerHtml.Contains("unavailable"))
-                    {
-                        Console.WriteLine(ServerName + " is unavailable :(");
-                    }
-                    else
-                    {
-                        Console.WriteLine(ServerName + " is available!!! GO GO GO!!!");
-                        SendSuccessEmail();
-                    }
+					if (characterCreationStatusItems[0].InnerHtml.Contains("world-ic__unavailable"))
+					{
+						Console.WriteLine(ServerName + " is unavailable :(");
+					}
+					else if (characterCreationStatusItems[0].InnerHtml.Contains("world-ic__available"))
+					{
+						Console.WriteLine(ServerName + " is available!!! GO GO GO!!!");
+						SendSuccessEmail();
+					}
+					else
+					{
+						Console.WriteLine(ServerName + " is under maintenance");
+					}
 
-                    return;
-                }
-            }
+					return;
+				}
+			}
 
-            Console.WriteLine("We couldn't find Tonberry :(");
-        }
+			Console.WriteLine("We couldn't find Tonberry :(");
+		}
 
-        private static void SendTestEmail()
-        {
-            SendEmail
-                (
-                    "Hello Adventurer! Kupo kupo!\n" +
-                    "\n" +
-                    "From your friend, the Messenger Moogle"
-                );
-        }
+		private static void SendTestEmail()
+		{
+			SendEmail
+				(
+					"Hello Adventurer! Kupo kupo!\n" +
+					"\n" +
+					"From your friend, the Messenger Moogle"
+				);
+		}
 
-        private static void SendSuccessEmail()
-        {
-            SendEmail
-                 (
-                    "Tonberry is available kupo!!\n" +
-                    "\n" +
-                    "From your friend, the Messenger Moogle"
-                 );
-        }
+		private static void SendSuccessEmail()
+		{
+			SendEmail
+				 (
+					"Tonberry is available kupo!!\n" +
+					"\n" +
+					"From your friend, the Messenger Moogle"
+				 );
+		}
 
-        private static void SendEmail(string body)
-        {
-            EmailConfiguration emailConfiguration = LoadEmailCredentialsFromFile();
+		private static void SendEmail(string body)
+		{
+			EmailConfiguration emailConfiguration = LoadEmailCredentialsFromFile();
 
-            MailMessage email = new MailMessage();
+			MailMessage email = new MailMessage();
 
-            email.From = new MailAddress(emailConfiguration.Sender.Email);
-            email.To.Add(emailConfiguration.Receiver.Email);
-            email.Body = body;
+			email.From = new MailAddress(emailConfiguration.Sender.Email);
+			email.To.Add(emailConfiguration.Receiver.Email);
+			email.Body = body;
 
-            SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+			SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
 
-            smtpServer.Port = 587;
-            smtpServer.Credentials = new System.Net.NetworkCredential(emailConfiguration.Sender.Email, emailConfiguration.Sender.Password);
-            smtpServer.EnableSsl = true;
+			smtpServer.Port = 587;
+			smtpServer.Credentials = new System.Net.NetworkCredential(emailConfiguration.Sender.Email, emailConfiguration.Sender.Password);
+			smtpServer.EnableSsl = true;
 
-            smtpServer.Send(email);
-        }
+			smtpServer.Send(email);
+		}
 
-        class EmailConfiguration
-        {
-            public SenderDetails Sender;
-            public ReceiverDetails Receiver;
+		class EmailConfiguration
+		{
+			public SenderDetails Sender;
+			public ReceiverDetails Receiver;
 
-            public class SenderDetails
-            {
-                public string Email = "";
-                public string Password = "";
-            }
+			public class SenderDetails
+			{
+				public string Email = "";
+				public string Password = "";
+			}
 
-            public class ReceiverDetails
-            {
-                public string Email = "";
-            }
-        }
+			public class ReceiverDetails
+			{
+				public string Email = "";
+			}
+		}
 
-        private static EmailConfiguration LoadEmailCredentialsFromFile()
-        {
-            string fileName = "EmailConfiguration.json";
+		private static EmailConfiguration LoadEmailCredentialsFromFile()
+		{
+			string fileName = "EmailConfiguration.json";
 
-            string fileText = File.ReadAllText(fileName);
+			string fileText = File.ReadAllText(fileName);
 
-            EmailConfiguration credentialsFromFile = JsonConvert.DeserializeObject<EmailConfiguration>(fileText);
+			EmailConfiguration credentialsFromFile = JsonConvert.DeserializeObject<EmailConfiguration>(fileText);
 
-            return credentialsFromFile;
-        }
-    }
+			return credentialsFromFile;
+		}
+	}
 }
